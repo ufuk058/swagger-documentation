975f187d1878ae9bc6504774c28e8dd3c3440a62
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-16 13:46:17 +0000

    More information added to Swagger Documentation

--- src/main/java/com/swagger/SwaggerDocumentationApplication.java
+++ src/main/java/com/swagger/SwaggerDocumentationApplication.java
@@ -1,10 +1,46 @@
 package com.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "My Swagger Application",
+				description = "Swagger Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Kiron",
+						email= "kiron@gmail.com",
+						url = "https://moonstar.com"
+
+				)
+		),
+		servers = {
+			@Server(
+					url = "dev.moonstar.com",
+					description = "Developer Server"
+			),
+			@Server(
+					url = "staging.moonstar.com",
+					description = "Staging Server"
+			),
+			@Server(
+					url = "qa.moonstar.com",
+					description = "Testing Server"
+			),
+			@Server(
+					url = "prod.moonstar.com",
+					description = "Production Server"
+			)
+
+		}
+)
 @SpringBootApplication
 public class SwaggerDocumentationApplication {
 
